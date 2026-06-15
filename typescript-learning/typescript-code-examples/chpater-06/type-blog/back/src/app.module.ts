5925ecad91553170f692e2617b080e3edfab33d8
webmooncity-personal-macbook <webmooncity@gmail.com>
2026-06-16 01:31:57 +0900

    Update: chapter 6.3.3 API

--- typescript-learning/typescript-code-examples/chpater-06/type-blog/back/src/app.module.ts
+++ typescript-learning/typescript-code-examples/chpater-06/type-blog/back/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MulterModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- typescript-learning/typescript-code-examples/chpater-06/type-blog/back/src/main.ts
+++ typescript-learning/typescript-code-examples/chpater-06/type-blog/back/src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as session from 'express-session';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestApplication>(AppModule);
+
+  app.use(
+    session({
+      secret: 'password',
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge: 60 * 60 * 1000 },
+    }),
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'images'));
+
+  await app.listen(3000);
 }
 bootstrap();
